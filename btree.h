b137baaa2a07f072719b85dc023ce8cc7752a602
cristianolacerda <clacerda.cristiano@gmail.com>
2016-06-14 22:24:11 -0300

    fixed btree.h

--- btree.h
+++ btree.h
@@ -1,29 +1,21 @@
-/*
- Cristiano Carvalho Lacerda: 8531737
- Joice  Aurino Lima: 8530851
- Leando Kondo:8921291
- */
 #ifndef __FileStructs__btree__
 #define __FileStructs__btree__
 
 #include <stdio.h>
 #include <stdint.h>
 
-
-/* Exported types */
-
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
 
+/* Exported types */
 /* User can modify this to add any desirable satelite info for a key */
-/* MUST NOT HAVE ANY POINTERS and must keep a trivial comparable entry named key */
+/* MUST NOT HAVE ANY POINTERS and must keep key */
 /* Note that the size of this struct will affect the tree order */
 typedef struct _bt_payload {
     u32 key;
-    u32 value;      /* This will be the offset of a key in the register file */
-    u32 unused;     /* this is here to force the order of the tree to be 4 */
+    u32 value;
 } __attribute__((packed)) bt_payload;
 
 /* The BTHANDLE type represents an reference to the b tree
@@ -39,7 +31,7 @@ BTHANDLE bt_create(const char *name, unsigned int pageSize);
 BTHANDLE bt_open(const char *name);
 int bt_put(BTHANDLE, bt_payload entry);
 bt_payload* bt_get(BTHANDLE h, int key);
-//int bt_delete(BTHANDLE, int key); TODO
+//int bt_delete(BTHANDLE, int key); 
 bt_payload bt_nextRecord(BTHANDLE);
 
 
